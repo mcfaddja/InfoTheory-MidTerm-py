dfb20789e35a8c72a10ac8b957fd8f2d7b139742
mcfaddja <mcfaddja@uw.edu>
2018-02-12 02:44:27 -0800

    done working 2-11-2018

---
+++ prob3/FromBook/lz.py
@@ -0,0 +1,61 @@
+def lzCompr(textIn):
+	
+	myDict = dict()
+	myAns = []
+	myResult = ''
+	myComp = ''
+	
+	
+	posCTR = 1
+	word = ''
+	for char in textIn:
+		wordNchar = word + char
+		
+		if (wordNchar in myDict):
+			word = wordNchar
+		else:
+			myDict[wordNchar] = posCTR
+			posCTR += 1
+			
+			if (len(wordNchar) == 1):
+				myAns.append([0, char])
+			else:
+				anINT = myDict[word]
+				myAns.append([anINT, char])
+				
+			word = ''
+			
+			
+	if (word):
+		anINT = myDict[word]
+		myAns.append([anINT])
+		
+	
+	for row in myAns:
+		for col in row:
+			myResult = myResult + str(col)
+			
+	
+	for row in myAns:
+		tst = 0
+		
+		for col in row:
+			
+			if tst == 0:
+				myComp = myComp + str(col) + ','
+			else:
+				myComp = myComp + str(col) + ';'
+					
+			tst += 1
+	
+	
+	
+	myOut = [myAns, myResult, myComp]	
+	
+	#return myResult
+	return myOut
+	
+	
+	
+#def lzDEcompr(comprTextIn):
+	
